ee7c74c963aa3376371c4d20c0ec50ffdfa720f6
Mark Chavez <los_populares@yahoo.com>
2013-06-02 19:46:38 +0800

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def portfolio
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metrorails::Application.config.secret_token = '887311a85a3fc97be1193a161315f4f46aac6a4313d2b16f8f13f1facd990bef2d1c2f5112b540a386d173301b1f0fb8a32267a913bde4d98309ad5d004ac603'
